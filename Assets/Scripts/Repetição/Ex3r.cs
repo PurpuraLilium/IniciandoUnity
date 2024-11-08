4f1a76f879a5010e8cfbd442cc0d662c352b6427
PurpuraLilium <Gomesyuridasilva@gmail.com>
2024-11-08 16:44:08 -0300

    feat: eX2 loop

--- Assets/Scripts/Repetição/Ex3r.cs
+++ Assets/Scripts/Repetição/Ex3r.cs
@@ -2,18 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ex2r : MonoBehaviour
+public class Ex3r : MonoBehaviour
 {
-    int dano = 5;
-    int atk = 1;
+    int fase = 1;
+    int coin = 0;
     void Start()
     {
-        
-        while (atk <= 5)
+        while (fase <= 10)
         {
-            print(dano);
-            atk++;
-            dano += 2;
+            print("Moedas coletadas:");
         }
     }
 

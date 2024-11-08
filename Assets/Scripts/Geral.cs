d6452f38364255c007ea1a89b5c422e27220e8d9
PurpuraLilium <Gomesyuridasilva@gmail.com>
2024-11-08 16:30:14 -0300

    feat: Ex1 loop

--- Assets/Scripts/Geral.cs
+++ Assets/Scripts/Geral.cs
@@ -20,6 +20,8 @@ public class Geral : MonoBehaviour
 
     bool = Verdadeiro ou falso
 
+    ---------------------------------
+
     public = variavel que aparece e pode ser chamada em outros scripts
 
     private = variavel que só é visivel no proprio script
@@ -36,6 +38,79 @@ public class Geral : MonoBehaviour
 
     variavel = condição ? se verdade : se falso;
      */
+
+    /*
+     ARRAY
+
+    tipo[] Variavel = {x , y , z , ...};
+    ex:
+
+    string[] nomes;
+    nomes = new string[6]; //define o tamanho da lista
+    nomes[0] = "Tadeu";
+
+    ---------------------------------
+
+    List<int> variavel = new List<int>()
+
+    variavel.Add(coisa);
+    veriavel.Remove(coisa);
+    variavel.Sort //organiza a lista em ordem crescente
+    variavel.Reverse //organiza a lista em ordem decrescente
+    variavel.(IndexOf(itemdalista) //mostra o indice do elemnto da lista
+
+     */
+
+
+    /*
+     LOOP
+
+    for(Inicializador ; condição ; incremento/decremento)
+    ex:
+        for(int i = 0 ; i <= 10 ; i++)
+    {
+        print(i);
+    }
+
+    ---------------------------------
+
+    while(condição)
+    {
+        ação a se executar;
+    }
+    ex:
+        while(x <= 10)
+    {
+            print(x);
+            x++;
+    }
+
+    ---------------------------------
+
+    do
+    {
+    (ação a se executar)
+    }
+    while(condição);
+    ex:
+    do
+    {
+        print(y);
+        y--
+    }
+    while(y <= 0);
+
+    ---------------------------------
+
+    foreach (var variavel1 in variavel2)
+    {
+    print(variavel1)
+    }
+     */
+
+    /*
+     
+    */
 
 
     void Start()
---
+++ Assets/Scripts/Repetição/Ex1r.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ex1r : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int cont = 5; cont >= 1; cont--)
+        {
+            print(cont);
+        }
+        print("Início da partida!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

4d3f3c290dddf3fd37057f8caf1e9ba498d3b974
PurpuraLilium <Gomesyuridasilva@gmail.com>
2024-11-11 15:23:07 -0300

    feat: ex7 loop

---
+++ Assets/Scripts/Repetição/Ex7r.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Ex7r : MonoBehaviour
+{
+    int[] numeros = {0 , 10 , 37 , 57 , 21};
+    int num;
+
+    
+    
+    void Start()
+    {
+        foreach (int i in numeros)
+        {
+            if (num < i)
+            {
+                num = i;
+            }
+        }
+        print(num);
+
+
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
